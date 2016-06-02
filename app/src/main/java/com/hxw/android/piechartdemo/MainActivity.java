1d9d9c61ce83ce1fad8b3903d2863731b12c624b
tony.hu <tony.hu@eku001.com>
2016-06-02 19:11:04 +0800

    初始项目。

---
+++ app/src/main/java/com/hxw/android/piechartdemo/MainActivity.java
@@ -0,0 +1,51 @@
+package com.hxw.android.piechartdemo;
+
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    private static Random random = new Random();
+    private PieChart pie_chart1, pie_chart2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+       pie_chart1 = (PieChart) findViewById(R.id.pie_chart1);
+        pie_chart2 = (PieChart) findViewById(R.id.pie_chart2);
+        pie_chart2.setAnimMode(PieChart.ANIM_MODE_MULTI_SECTION);
+       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                refreshPieChart();
+            }
+        });
+        refreshPieChart();
+    }
+
+    private void refreshPieChart() {
+        ArrayList<Map.Entry<Double, Integer>> pieData = new ArrayList<>();
+        pieData.add(new AbstractMap.SimpleEntry<Double, Integer>(random.nextInt(5000) + 0d, 0xFF71dabe));
+        pieData.add(new AbstractMap.SimpleEntry<Double, Integer>(random.nextInt(5000) + 0d, 0xFFff6656));
+        pieData.add(new AbstractMap.SimpleEntry<Double, Integer>(random.nextInt(5000) + 0d, 0xFF7bbef8));
+        pieData.add(new AbstractMap.SimpleEntry<Double, Integer>(random.nextInt(5000) + 0d, 0xFF6b96f5));
+        pie_chart1.setPieData(pieData);
+        pie_chart2.setPieData(pieData);
+    }
+}
---
+++ app/src/main/java/com/hxw/android/piechartdemo/PieChart.java
@@ -0,0 +1,192 @@
+package com.hxw.android.piechartdemo;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+/**
+ * 饼状图控件。
+ * Created by tony.hu on 2016/5/17.
+ */
+public class PieChart extends View {
+    /**
+     * 饼状图的动画方式：整个环从起点到逐渐画满360度。
+     */
+    public static final int ANIM_MODE_SINGLE_SECTION = 1;
+    /**
+     * 饼状图的动画方式：组成整个360度环的各个环分别从其起点画满其弧度。
+     */
+    public static final int ANIM_MODE_MULTI_SECTION = 2;
+    private Paint mPaintOuter, mPaintInner;
+    private RectF mBigOval, mSmallOval;
+    private int[] colors;
+    private float[] angles;
+    private final float DEFAULT_WIDTH_FACTOR = 0.3529f;
+    private float mRingWidthFactor = DEFAULT_WIDTH_FACTOR;
+    private final int animDuration = 600;
+    /** 当动画开始时间重置为0时，在onDraw时会开始动画绘制。 */
+    private long animStartTime = 0L;
+    private float startAngle = 0f;
+    private int animMode = ANIM_MODE_SINGLE_SECTION;
+
+    public PieChart(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initPaints();
+    }
+
+    private void initPaints() {
+        mPaintOuter = new Paint();
+        mPaintOuter.setAntiAlias(true);
+        mPaintOuter.setStyle(Paint.Style.FILL);
+        mPaintOuter.setColor(Color.WHITE);
+
+        mPaintInner = new Paint(mPaintOuter);
+        mPaintInner.setColor(Color.WHITE);
+    }
+
+    /**
+     * 设置饼状图的动画方式。
+     * @param mode 常量ANIM_MODE_XXX。
+     */
+    public void setAnimMode(int mode) {
+        if (mode < 1) {
+            mode = 1;
+        } else if (mode > 2) {
+            mode = 2;
+        }
+        animMode = mode;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawColor(Color.TRANSPARENT);
+        if (colors == null) return;
+        if (mBigOval == null) {
+            setRingWidthFactor(mRingWidthFactor);
+        }
+
+        if (animStartTime == 0) animStartTime = System.currentTimeMillis();
+        long duration = System.currentTimeMillis() - animStartTime;
+        if (duration > animDuration) {
+            duration = animDuration;
+        }
+        float progress = duration / (animDuration + 0f);
+
+        if (animMode == ANIM_MODE_SINGLE_SECTION) {
+            animDrawSingle(canvas, progress);
+        } else {
+            animDrawMulti(canvas, progress);
+        }
+
+        // 画内部圆，形成环。
+        canvas.drawArc(mSmallOval, 0, 360, true, mPaintInner);
+
+        if (duration < animDuration) {
+            invalidate();
+        }
+    }
+
+    /**
+     * 设置饼状图绘制各个分段的起点角度，X轴正方向为0度。
+     * @param startAngle
+     */
+    public void setStartAngle(float startAngle) {
+        this.startAngle = startAngle;
+    }
+
+    /**
+     * 根据进度值，以动画方式画饼状图。动画方式：各个分段分别从0%-100%进行绘制。
+     * @param canvas
+     * @param progress
+     */
+    private void animDrawMulti(Canvas canvas, float progress) {
+        float start = startAngle;
+        float sweep;
+        for (int i = 0; i < angles.length; i++) {
+            sweep = (float) (angles[i] * progress + 0.5f);
+            mPaintOuter.setColor(colors[i]);
+            canvas.drawArc(mBigOval, start, sweep, true, mPaintOuter);
+            start += angles[i];
+        }
+    }
+
+    /**
+     * 根据进度值，以动画方式画饼状图。动画方式：整个环从0-360度进行绘制。
+     * @param canvas
+     * @param progress
+     */
+    private void animDrawSingle(Canvas canvas, float progress) {
+        float start = startAngle;
+        float sweep;
+        float end = start + 360f * progress;
+        for (int i = 0; i < angles.length; i++) {
+            sweep = (float) (angles[i] + 0.5f);
+            mPaintOuter.setColor(colors[i]);
+            if (sweep + start <= end) {
+                canvas.drawArc(mBigOval, start, sweep, true, mPaintOuter);
+            } else {
+                sweep = end - start;
+                canvas.drawArc(mBigOval, start, sweep, true, mPaintOuter);
+                break;
+            }
+            start += angles[i];
+        }
+    }
+
+    /**
+     * 设置饼状图数据。格式为“数值-颜色”的列表。
+     */
+    public void setPieData(ArrayList<Map.Entry<Double, Integer>> pieList) {
+        ArrayList<Map.Entry<Double, Integer>> tempPieList = pieList;
+        if (tempPieList == null) {
+            tempPieList = new ArrayList<>();
+        }
+
+        double total = 0;
+        colors = new int[tempPieList.size()];
+
+        for (int i = 0; i < tempPieList.size(); i++) {
+            total += tempPieList.get(i).getKey();
+            colors[i] = tempPieList.get(i).getValue();
+        }
+
+        angles = new float[tempPieList.size()];
+        for (int i = 0; i < tempPieList.size(); i++) {
+            angles[i] = (float) (tempPieList.get(i).getKey() / total) * 360f;
+        }
+
+        animStartTime = 0L;
+        invalidate();
+    }
+
+    private void updateBounds() {
+        int width = getMeasuredWidth();
+        int height = getMeasuredHeight();
+        int diameter = width > height ? height : width;
+        int radius = diameter / 2;
+        mBigOval = new RectF(0, 0, diameter, diameter);
+        mSmallOval = new RectF(radius * mRingWidthFactor,
+                radius * mRingWidthFactor,
+                diameter - radius * mRingWidthFactor,
+                diameter - radius * mRingWidthFactor);
+    }
+
+    /**
+     * 设置环的宽度参数factor，设置factor并不会引起View重新绘制，如果需要，调用invalidate()。
+     * @param factor 表示环的宽度和外圆半径的比值。
+     */
+    public void setRingWidthFactor(float factor) {
+        if (factor <= 0 || factor >= 1) {
+            factor = DEFAULT_WIDTH_FACTOR;
+        }
+        mRingWidthFactor = factor;
+        updateBounds();
+    }
+}
